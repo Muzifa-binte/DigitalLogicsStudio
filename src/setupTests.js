93a57a96a2328f214f07bdd595bea0cc7af38a6a
SENODROOM <136210526+SENODROOM@users.noreply.github.com>
2026-05-29 17:28:49 +0500

    fixed the whale issues

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,12 +2,4 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
-
-class ResizeObserverMock {
-  observe() {}
-  unobserve() {}
-  disconnect() {}
-}
-
-global.ResizeObserver = global.ResizeObserver || ResizeObserverMock;
+import "@testing-library/jest-dom";
